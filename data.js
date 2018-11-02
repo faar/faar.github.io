b6ffb793d12a2455b2bd23c16e5319180f168114
faar <44703287+faar@users.noreply.github.com>
2018-11-02 15:15:09 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,19 @@
-function init() {
- Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/1ucLccgU4xJSKYN4NGnienWFhgMW_ipyOp0AREwEBz2U/edit?usp=sharing',
- callback: function(data, tabletop) { 
- console.log(data)
- },
- simpleSheet: true } )
-}
-window.addEventListener(‘DOMContentLoaded’, init)
+<script src='https://cdnjs.cloudflare.com/ajax/libs/tabletop.js/1.5.1/tabletop.min.js'></script>
+<script type='text/javascript'>    
+  var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1ucLccgU4xJSKYN4NGnienWFhgMW_ipyOp0AREwEBz2U/edit?usp=sharing';
+
+  function init() {
+    Tabletop.init( { key: publicSpreadsheetUrl,
+                     callback: showInfo,
+                     simpleSheet: true } )
+  }
+
+  function showInfo(data, tabletop) {
+    alert('Successfully processed!')
+    console.log(data);
+  }
+
+  window.addEventListener('DOMContentLoaded', init)
+</script>
+
+
